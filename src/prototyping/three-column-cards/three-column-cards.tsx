3eadc21d44e3013b389642e324a5f7cc9249d2b7
Jake Love <jake@basalt.io>
2021-03-10 14:02:02 -0800

    feat: add dark mode to three column cards prototype

--- src/prototyping/three-column-cards/three-column-cards.tsx
+++ src/prototyping/three-column-cards/three-column-cards.tsx
@@ -7,7 +7,7 @@ import Author from '../../components/author/author';
 export const ThreeColumnCards = () => {
   return (
     <>
-    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 dark:bg-gray-700">
       <div className="absolute inset-0">
         <div className="bg-white h-1/3 sm:h-2/3"></div>
       </div>
@@ -16,7 +16,7 @@ export const ThreeColumnCards = () => {
           <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
             From the blog
           </h2>
-          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4 dark:text-gray-400">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
           </p>
         </div>
