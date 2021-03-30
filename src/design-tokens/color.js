4d11e9b21a256dd59239345cf14332e3316ddb09
Jake Love <jake@basalt.io>
2021-03-30 15:20:09 -0700

    feat: new button pattern

--- src/design-tokens/color.js
+++ src/design-tokens/color.js
@@ -1,4 +1,31 @@
 // Core Color Values
+const base = {
+  yellow: { value: '#ffd94f' },
+  green: { value: '#3ccf91' },
+  blue: { value: '#3c5ccf' },
+  orange: { value: '#ff934f' },
+  red: { value: '#ef5151' },
+  orchid: { value: '#aa1baf' },
+}
+
+const light = {
+  yellow: { value: '#ffeeb3' },
+  green: { value: '#8de2bf' },
+  blue: { value: '#8d9fe2' },
+  orange: { value: '#ffd0b3' },
+  red: { value: '#f8b0b0' },
+  orchid: { value: '#e77bea' },
+}
+
+const dark = {
+  yellow: { value: '#e6b400' },
+  green: { value: '#21835a' },
+  blue: { value: '#213683' },
+  orange: { value: '#e65800' },
+  red: { value: '#c91313' },
+  orchid: { value: '#560e58' },
+}
+
 const layout = {
   defaultBg: { value: '#FFFFFF' },
   primary: { value: '#393B3E' },
@@ -15,6 +42,9 @@ const text = {
 
 module.exports = {
   color: {
+    base,
+    light,
+    dark,
     layout,
     text
   },
