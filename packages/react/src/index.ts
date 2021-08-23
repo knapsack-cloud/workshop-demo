44884ef6579060f3ede987aeb83083715f60deeb
Jake Love <jake@basalt.io>
2021-08-23 15:56:23 -0700

    feat: add top nav component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,4 +3,4 @@ export * from './button/button';
 export * from './card/card';
 // export * from './hero/hero';
 // export * from './layout/layout';
-// export * from './top-nav/top-nav';
+export * from './top-nav/top-nav';
