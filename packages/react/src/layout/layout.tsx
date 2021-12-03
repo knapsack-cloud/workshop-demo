90142994f23ad57eed7a7ec640983bf5551535ad
Jake Love <jake@knapsack.cloud>
2021-12-03 10:39:26 -0800

    fix: darkmode - temp hack

--- packages/react/src/layout/layout.tsx
+++ packages/react/src/layout/layout.tsx
@@ -8,7 +8,8 @@ export const Layout: React.FC<LayoutProps> = ({
 }: LayoutProps) => {
 
   const childrenCount = children ? React.Children.count(children) : 1;
-
+  // NOTE: Tailwind will find the below and not purge the dark: mode styles
+  // Tailwind: <div className="dark"></div>
   return (
     <>
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 dark:bg-gray-700">
