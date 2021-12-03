8b8e54004ad28108f2f7ef13090ab172bef2ebad
Jake Love <jake@knapsack.cloud>
2021-12-02 16:38:55 -0800

    fix: responsive grid styling

--- packages/react/src/layout/layout.tsx
+++ packages/react/src/layout/layout.tsx
@@ -6,35 +6,38 @@ export const Layout: React.FC<LayoutProps> = ({
   bandContent,
   children
 }: LayoutProps) => {
+
+  const childrenCount = children ? React.Children.count(children) : 1;
+
   return (
     <>
-    <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 dark:bg-gray-700">
-      <div className="absolute inset-0">
-        <div className="bg-white h-1/3 sm:h-2/3"></div>
+      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 dark:bg-gray-700">
+        <div className="absolute inset-0">
+          <div className="bg-white h-1/3 sm:h-2/3"></div>
+        </div>
+        <div className="relative max-w-7xl mx-auto">
+          {(bandTitle || bandContent) && (
+            <div className="text-center">
+              {bandTitle && (
+                <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+                  {bandTitle}
+                </h2>
+              )}
+              {bandContent && (
+                <div className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4 dark:text-gray-400">
+                  {bandContent}
+                </div>
+              )}
+            </div>
+          )}
+          {children && (
+            <div className={`mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-${childrenCount} lg:max-w-none`}>
+              {children}
+            </div>
+          )}
+        </div>
       </div>
-      <div className="relative max-w-7xl mx-auto">
-        {(bandTitle || bandContent) && (
-          <div className="text-center">
-            {bandTitle && (
-              <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-                {bandTitle}
-              </h2>
-            )}
-            {bandContent && (
-              <div className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4 dark:text-gray-400">
-                {bandContent}
-              </div>
-            )}
-          </div>
-        )}
-        {children && (
-          <div className={`mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-${React.Children.count(children) ? React.Children.count(children) : 1} lg:max-w-none`}>
-            {children}
-          </div>
-        )}
-      </div>
-    </div>
-  </>
+    </>
   );
 };
 
--- packages/styles/tailwind.config.js
+++ packages/styles/tailwind.config.js
@@ -1,14 +1,19 @@
 const { color, breakpoint, spacing } = require('@knapsack-cloud/demo-design-system-design-tokens/dist/design-tokens.nested.json');
 
 module.exports = {
-  purge: [
-    '../angular/src/lib/**/*.html',
-    '../html/**/*.html',
-    '../react/src/**/*.tsx',
-    '../twig/src/**/*.twig',
-    '../vue/src/**/*.vue',
-    '../web-components/src/**/*.wc.js',
-  ],
+  purge: {
+    content: [
+      '../angular/src/lib/**/*.html',
+      '../html/**/*.html',
+      '../react/src/**/*.tsx',
+      '../twig/src/**/*.twig',
+      '../vue/src/**/*.vue',
+      '../web-components/src/**/*.wc.js'
+    ],
+    safelist: [
+      /grid-cols-.*/
+    ]
+  },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
