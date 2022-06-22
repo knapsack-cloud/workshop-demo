983e0a1fe631db548680b5a3c1464d32fb82cdc1
Jake Love <jake@knapsack.cloud>
2022-06-22 16:49:10 -0700

    feat: new footer component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,6 +2,7 @@ export * from './author/author';
 export * from './button/button';
 export * from './card/card';
 export * from './card/cardDataDemo';
+export * from './footer/footer';
 export * from './hero/hero';
 export * from './layout/layout';
 export * from './prototype/one-pager/page';
