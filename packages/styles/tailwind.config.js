300b1f10ad2dc783dddab8851215d6744d7ee20d
Christopher Bloom <bloomcb@gmail.com>
2022-09-16 15:04:27 -0700

    fonts beginning logo

--- packages/styles/tailwind.config.js
+++ packages/styles/tailwind.config.js
@@ -1,9 +1,16 @@
-const { color, breakpoint, spacing } = require('@knapsack-cloud/demo-design-system-design-tokens/dist/design-tokens.nested.json');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const {
+  color,
+  breakpoint,
+  spacing,
+} = require('@knapsack-cloud/demo-design-system-design-tokens/dist/design-tokens.nested.json');
 
 module.exports = {
   purge: {
     content: [
       { raw: '<body class="dark"></div>', extension: 'html' },
+      { raw: '<body class="wow"></div>', extension: 'html' },
       '../angular/src/lib/**/*.html',
       '../html/**/*.html',
       '../react/src/**/*.tsx',
@@ -11,20 +18,21 @@ module.exports = {
       '../vue/src/**/*.vue',
       '../web-components/src/**/*.wc.js',
     ],
-    safelist: [
-      /grid-cols-.*/
-    ]
+    safelist: [/grid-cols-.*/],
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       colors: color,
       screens: breakpoint.width,
-      spacing
+      spacing,
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
